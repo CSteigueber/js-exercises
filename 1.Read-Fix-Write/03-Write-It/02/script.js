2c275984cad6fc17283cd3b71be42fb6ae0e3ad1
CSteigueber <c.steigueber@gmail.com>
2019-10-30 16:53:49 +0100

    second ex not working yet

---
+++ 1.Read-Fix-Write/03-Write-It/02/script.js
@@ -0,0 +1,10 @@
+var films=[`Hors Normes`,`The Mustang`,`Joker`,`Downtown Abbey`,`Parasite`,`La Ragazzo Nella Nebbia`, 
+`Once upon a time ... in Hollywood`,`Au Nom De La Terre`,`ITZHAK`, `Everest: De Jonge Yeti`, `Minuscule 2: het tropische avontuur`,
+`De Leeuwenkoning`];
+
+
+const toLower = (content) => content.toLowerCase();
+const wrapWithTag = (content, tag)=> `<${tag}>${content}</${tag}>`;
+fimls= films.map(film => toLower(film));
+films= films.map(film => wrapWithTag(film,`li`));
+document.write(wrapWithTag(films.join(``),`ul`));
