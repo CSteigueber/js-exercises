6c2413e7e10d5653a04bc8c9ead8cf73f4bd3624
CSteigueber <c.steigueber@gmail.com>
2019-10-30 16:45:09 +0100

    initial commit

---
+++ 1.Read-Fix-Write/03-Write-It/01/script.js
@@ -0,0 +1,7 @@
+var films=[`Hors Normes`,`The Mustang`,`Joker`,`Downtown Abbey`,`Parasite`,`La Ragazzo Nella Nebbia`, 
+`Once upon a time ... in Hollywood`,`Au Nom De La Terre`,`ITZHAK`, `Everest: De Jonge Yeti`, `Minuscule 2: het tropische avontuur`,
+`De Leeuwenkoning`];
+
+const wrapWithTag = (content, tag)=> `<${tag}>${content}</${tag}>`;
+films= films.map(film => wrapWithTag(film,`li`));
+document.write(wrapWithTag(films.join(``),`ul`));
