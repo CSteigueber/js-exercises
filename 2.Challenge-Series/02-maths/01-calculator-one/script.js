154939545007906a804d4e71c0ea7ae834122b4b
CSteigueber <c.steigueber@gmail.com>
2019-10-31 15:00:18 +0100

    challenge math 1 done

--- 2.Challenge-Series/02-maths/01-calculator-one/script.js
+++ 2.Challenge-Series/02-maths/01-calculator-one/script.js
@@ -14,17 +14,21 @@
 
     document.getElementById("addition").addEventListener("click", function() {
         // perform an addition
+        alert(parseInt(document.getElementById("op-one").value)+ parseInt(document.getElementById("op-two").value));
     });
 
     document.getElementById("substraction").addEventListener("click", function() {
         // perform an substraction
+        alert(parseInt(document.getElementById("op-one").value)- parseInt(document.getElementById("op-two").value));
     });
 
     document.getElementById("multiplication").addEventListener("click", function() {
         // perform an multiplication
+        alert(parseInt(document.getElementById("op-one").value)* parseInt(document.getElementById("op-two").value));
     });
 
     document.getElementById("division").addEventListener("click", function() {
         // perform an division
+        alert(parseInt(document.getElementById("op-one").value)/ parseInt(document.getElementById("op-two").value));
     });
 })();
