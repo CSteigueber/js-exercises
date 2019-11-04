e1fc7f41387ddc07dc13d4c1191f4df0da6d3ac3
CSteigueber <c.steigueber@gmail.com>
2019-11-04 16:00:34 +0100

    date ex 5 done

--- 2.Challenge-Series/04-dates/05-get-spooky-fridays/script.js
+++ 2.Challenge-Series/04-dates/05-get-spooky-fridays/script.js
@@ -12,5 +12,34 @@
 (function() {
 
     // your code here
+    document.getElementById("run").addEventListener("click", function (){
+        let year=parseInt(document.getElementById("year").value);
+        let res=[];
+        for (let i=0;i<12;i++){
+            let date= new Date (year,i,13);
+            let wk=date.getDay();
+            let str="";
+            if (wk==5){
+                switch (i+1){
+                    case 1: str="January";      break;
+                    case 2: str="February";     break;
+                    case 3: str="March";        break;
+                    case 4: str="April";        break;
+                    case 5: str="May";          break;
+                    case 6: str="June";         break;
+                    case 7: str="July";         break;
+                    case 8: str="August";       break;
+                    case 9: str="September";    break;
+                    case 10: str="October";     break;
+                    case 11: str="November";    break;
+                    case 12: str="December";    break;
+                }
+                res.push(str);
+            }
+        }
+        alert(res);
+        
+    })
+        
 
 })();
