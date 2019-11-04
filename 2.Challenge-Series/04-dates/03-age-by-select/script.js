71e55d5cd84bea4858c052c26f85b7515bf6655d
CSteigueber <c.steigueber@gmail.com>
2019-11-04 14:45:11 +0100

    date ex 3 done

--- 2.Challenge-Series/04-dates/03-age-by-select/script.js
+++ 2.Challenge-Series/04-dates/03-age-by-select/script.js
@@ -12,5 +12,19 @@
 (function() {
 
     // your code here
+    document.getElementById("run").addEventListener("click", function (){
+        let dob_year= document.getElementById("dob-year").value;
+        let dob_month= document.getElementById("dob-month").value;
+        let dob_day= document.getElementById("dob-day").value;
+        let date= new Date();
+        let y=date.getFullYear();
+        let m=date.getMonth()+1;
+        let d=date.getDate();    
+        let age=y-dob_year;
+        if (m<dob_month ||(m==dob_month && d<dob_day)) age--;
+        alert("You are "+age+" years old.");
+
+
+    })
 
 })();
