621ef9094d918be8db740c1370e8a0e08ff5beec
CSteigueber <c.steigueber@gmail.com>
2019-11-05 17:00:32 +0100

    misc ex 2 done

--- 2.Challenge-Series/07-misc/02-typewriter-effect/script.js
+++ 2.Challenge-Series/07-misc/02-typewriter-effect/script.js
@@ -9,8 +9,32 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+var index=0;
+var delay =Math.random()*250;
+
+const typewriter = (str)=>{
+    let temp=str.slice(0,index%255);
+    document.getElementById("target").innerHTML=temp;
+    console.log(index+" "+temp+" delay: "+delay);
+    index++;
+    delay=Math.random()*250;
+
+}
+
+
 (function() {
 
     // your code here
+    var str=document.getElementById("target").innerHTML;
+    console.log(str.length);
+   /* for (let i=0; i<str.length; i++){
+        let delay= 250;
+        setTimeout(function () {typewriter(str,i)},delay);
+    }*/
+    setInterval(() => {
+        typewriter(str)        
+    }, delay);
+
+
 
 })();
