1ebadf120b17320c0f6276b5f11c7e845f43184c
CSteigueber <c.steigueber@gmail.com>
2019-11-08 15:01:52 +0100

    fetch ex 3 done

--- 2.Challenge-Series/09-fetch/03-details/script.js
+++ 2.Challenge-Series/09-fetch/03-details/script.js
@@ -15,7 +15,20 @@
     document.getElementById("run").addEventListener("click", async function(){
         var arr=await fetch("../../_shared/api.json");
         var preHeroes= await arr.json("/heroes");
-        var heroes=preHeroes.heroes;
-        console.table(heroes);
+        var hero=preHeroes.heroes;
+        //console.table(heroes);
+        var id= document.getElementById("hero-id").value;
+        var tpl=document.getElementById("tpl-hero").innerHTML;
+        var str="";
+        var name_pos=143;
+        var ego_pos=207;
+        var skill_pos=293;
+        
+        str+=tpl.slice(0,name_pos)+hero[id].name;
+        str+=tpl.slice(name_pos,ego_pos)+hero[id].alterEgo;
+        str+=tpl.slice(ego_pos,skill_pos)+hero[id].abilities;
+        str+=tpl.slice(skill_pos,0);;
+
+        document.getElementById("target").innerHTML=str;
     })
 })();
