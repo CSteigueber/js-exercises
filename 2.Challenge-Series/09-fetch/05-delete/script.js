26999467690e30eba38171da3348e2051d7df86c
CSteigueber <c.steigueber@gmail.com>
2019-11-08 16:43:04 +0100

    fetch ex 5 not perfect but done

--- 2.Challenge-Series/09-fetch/05-delete/script.js
+++ 2.Challenge-Series/09-fetch/05-delete/script.js
@@ -11,4 +11,13 @@
 
 (() => {
     // your code here
+    document.getElementById("run").addEventListener("click", async function(){
+        var arr= await fetch("../../_shared/api.json");
+        var preHeroes= await arr.json("/heroes");
+        var heroes=preHeroes.heroes;
+        var id=document.getElementById("hero-id").value-1;
+        heroes.splice(id,1);
+        console.table(heroes);
+
+    })
 })();
