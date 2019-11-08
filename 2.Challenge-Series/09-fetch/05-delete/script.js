b28bff8901bc766c14ff807b7b2b48fd4aef63c7
CSteigueber <c.steigueber@gmail.com>
2019-11-08 16:58:24 +0100

    fetch ex 5 done better

--- 2.Challenge-Series/09-fetch/05-delete/script.js
+++ 2.Challenge-Series/09-fetch/05-delete/script.js
@@ -9,14 +9,20 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(() => {
+(async function() {
     // your code here
+    var arr= await fetch("../../_shared/api.json");
+    var preHeroes= await arr.json("/heroes");
+    var heroes=preHeroes.heroes;
+    console.table(heroes);
     document.getElementById("run").addEventListener("click", async function(){
-        var arr= await fetch("../../_shared/api.json");
-        var preHeroes= await arr.json("/heroes");
-        var heroes=preHeroes.heroes;
-        var id=document.getElementById("hero-id").value-1;
-        heroes.splice(id,1);
+        var id=document.getElementById("hero-id").value;
+        for (let i=0; i<heroes.length;i++){
+            if (heroes[i].id==id){
+                heroes.splice(i,1);
+
+            }
+        }
         console.table(heroes);
 
     })
